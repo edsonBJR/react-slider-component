b00ac82503e7db60f0d521cbd3677fd85f974638
edsonBJR <ebjr.tux@gmail.com>
2020-12-02 18:04:04 -0300

    Arquivos prontos.

---
+++ src/components/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1573950940509-d924ee3fd345?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Y2Fyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1514316703755-dca7d7d9d882?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGNhcnN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1493238792000-8113da705763?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNhcnN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1517672651691-24622a91b550?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzd8fGNhcnN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    }
+]
